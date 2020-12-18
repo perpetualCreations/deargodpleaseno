b14ab2a7361787d0539c684da3569a4e1e4d2306
tchen <tchen0584@gmail.com>
2020-12-17 22:59:39 -0500

    Typo fix for arguments object, dgpn command now passes args.

--- src/DEBIAN/usr/bin/deargodpleaseno/main.py
+++ src/DEBIAN/usr/bin/deargodpleaseno/main.py
@@ -65,19 +65,19 @@ elif parameters.uninstall is True:
     print("Uninstall complete.")
     terminate(0)
 else:
-    if arguments.expire is None:
+    if parameters.expire is None:
         config_fetch = configparser.ConfigParser
         config_fetch.read("/etc/deargodpleaseno/settings.cfg")
-        arguments.expire = config_fetch["expire"]["time"]
-    elif arguments.add is not None and isdir(arguments.add) is False and isfile(arguments.add) is False:
+        parameters.expire = config_fetch["expire"]["time"]
+    elif parameters.add is not None and isdir(parameters.add) is False and isfile(parameters.add) is False:
         raise SyntaxError("Item path was not specified!")
-    elif arguments.edit is not None and isdir(arguments.edit) is False and isfile(arguments.edit) is False:
+    elif parameters.edit is not None and isdir(parameters.edit) is False and isfile(parameters.edit) is False:
         raise SyntaxError("Item path was not specified!")
-    elif arguments.edit is not None and arguments.expire is None:
+    elif parameters.edit is not None and parameters.expire is None:
         raise SyntaxError("User requested item expiry edit, however no expiry time was specified! Use --bestbefore to specify one.")
-    elif arguments.remove is not None and isdir(arguments.remove) is False and isfile(arguments.remove) is False:
+    elif parameters.remove is not None and isdir(parameters.remove) is False and isfile(parameters.remove) is False:
         raise SyntaxError("Item path was not specified!")
-    elif arguments.add is None and arguments.edit is None and arguments.remove is None:
+    elif parameters.add is None and parameters.edit is None and parameters.remove is None:
         raise SyntaxError("No action was specified. Use --add, --edit, or --remove to specify one, remember to type in the item path after the parameter.")
     elif parameters.edit is not None:
         with open("/etc/deargodpleaseno/entries") as fetch:
@@ -96,7 +96,7 @@ else:
             rebuild.writelines(regenerated)
         pass
         capture = None
-        call("sudo rm -r " + parameters.expire + " | at now + " + str(arguments.expire) + " hours", shell = True, stdout = capture)
+        call("sudo rm -r " + parameters.expire + " | at now + " + str(parameters.expire) + " hours", shell = True, stdout = capture)
         with open("/etc/deargodpleaseno/entries", "w") as dump:
             dump.write(capture.split("\n")[1].split()[1] + "|||" + parameters.expire)
         pass
@@ -104,7 +104,7 @@ else:
         terminate(0)
     elif paramters.add is not None:
         capture = None
-        call("sudo rm -r " + parameters.add + " | at now + " + str(arguments.expire) + " hours", shell = True, stdout = capture)
+        call("sudo rm -r " + parameters.add + " | at now + " + str(parameters.expire) + " hours", shell = True, stdout = capture)
         with open("/etc/deargodpleaseno/entries", "w") as dump:
             dump.write(capture.split("\n")[1].split()[1] + "|||" + parameters.add)
         pass
