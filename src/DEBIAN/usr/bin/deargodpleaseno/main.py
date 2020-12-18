1dd8e84777790e4bc5f8088e6eda4984dbc3268f
tchen <tchen0584@gmail.com>
2020-12-17 22:11:36 -0500

    Implemented add, remove, and edit function. Deprecated empty files.

--- src/DEBIAN/usr/bin/deargodpleaseno/main.py
+++ src/DEBIAN/usr/bin/deargodpleaseno/main.py
@@ -5,21 +5,21 @@ main.py, does the following:
 - run install.py and uninstall.py
 - add and remove items from deargodpleaseno's managed folder
 - edit items from deargodpleaseno's managed folder
-- display help
-- check for updates
+
+TODO add safety checks, optimize imports, modularize into functions, merge install and uninstall into this file
 """
 
 import argparse
 import subprocess
+from os import remove
 from sys import exit as terminate
 
 arguments = argparse.ArgumentParser(description = "Operate deargodpleaseno, use dgpn --help for a list of commands.")
-arguments.add_argument("--install", dest = "install", action = "store_const", const = True, help = "Run install script for deargodpleaseno.")
-arguments.add_argument("--uninstall", dest = "uninstall", action = "store_const", const = True, help = "Run uninstall script for deargodpleaseno. You'll still have to use your package manager to fully remove deargodpleaseno.")
-arguments.add_argument("--scan", dest = "scan", action = "store_const", const = True, help = "Force scan of deargodpleaseno directory under webroot for new items. Will assume default expiry time.")
-arguments.add_argument("--add", dest = "add", type = str, help = "Add item to deargodpleaseno directory under webroot, by specifying path. If --bestbefore is not used in conjunction, default expiry time will be assumed.")
-arguments.add_argument("--edit", dest = "edit", type = str, help = "Edit expiry time for item in deargodpleaseno directory under webroot, by specifying path. Requires specified new expiry, use --bestbefore.")
-arguments.add_argument("--remove", dest = "remove", type = str, help = "Remove item from deargodpleaseno directory under webroot, by specifying path. This is a destructive action, the item will be deleted! If a directory is specified, any files and subdirectories under it will be deleted.")
+arguments.add_argument("--install", dest = "install", action = "store_const", const = True, help = "Install deargodpleaseno.")
+arguments.add_argument("--uninstall", dest = "uninstall", action = "store_const", const = True, help = "Uninstall deargodpleaseno. You'll still have to use your package manager to fully remove deargodpleaseno.")
+arguments.add_argument("--add", dest = "add", type = str, help = "Add item to atq for deletion, by specifying path. If --bestbefore is not used in conjunction, default expiry time will be assumed. This is a destructive action.")
+arguments.add_argument("--edit", dest = "edit", type = str, help = "Edit expiry time for item by specifying path. Requires specified new expiry, use --bestbefore.")
+arguments.add_argument("--remove", dest = "remove", type = str, help = "Removes item from atq for deletion, by specif.")
 arguments.add_argument("--bestbefore", dest = "expire", type = int, help = "Specify hours until expiry, optional for --add and required for --edit.")
 parameters = arguments.parse_args()
 
@@ -31,18 +31,50 @@ elif parameters.uninstall is True:
     print("Called uninstall.")
     subprocess.Popen("python uninstall.py", shell = True)
     terminate(0)
-elif parameters.scan is True:
-    print("Started scan for new items in dgpn's directory under webroot.")
-    subprocess.Popen("python scan.py", shell = True)
 elif parameters.expire is not None:
     if parameters.edit is not None:
+        with open("/etc/deargodpleaseno/entries") as fetch:
+            index = 0
+            while index <= len(fetch.read().split("\n")):
+                if fetch.read().split("\n")[index].split("|||")[1] == parameters.edit:
+                    subprocess.call("sudo atrm " + fetch.read().split("\n")[index].split("|||")[0])
+                    regenerated = fetch.read().split("\n")
+                    regenerated.remove(fetch.read().split("\n")[index].split("|||")[0] + "|||" + fetch.read().split("\n")[index].split("|||")[1])
+                pass
+            pass
+        pass
+        remove("/etc/deargodpleaseno/entries")
+        with open("/etc/deargodpleaseno/entries", "w") as rebuild:
+            rebuild.writelines(regenerated)
+        pass
+        capture = None
+        subprocess.call("sudo rm -r " + parameters.expire + " | at now + " + str(arguments.expire) + " hours", shell = True, stdout = capture)
+        with open("/etc/deargodpleaseno/entries", "w") as dump:
+            dump.write(capture.split("\n")[1].split()[1] + "|||" + parameters.expire)
+        pass
         print("Edited item expiry time.")
-        # TODO AT edit
     pass
 elif paramters.add is not None:
+    capture = None
+    subprocess.call("sudo rm -r " + parameters.add + " | at now + " + str(arguments.expire) + " hours", shell = True, stdout = capture)
+    with open("/etc/deargodpleaseno/entries", "w") as dump:
+        dump.write(capture.split("\n")[1].split()[1] + "|||" + parameters.add)
+    pass
     print("Added item.")
-    # TODO AT append
 elif parameters.remove is not None:
-    input("**This is a destructive action, the item will be deleted! Press enter to continue, otherwise press Ctrl+C.**")
-    # TODO AT edit
+    with open("/etc/deargodpleaseno/entries") as fetch:
+        index = 0
+        while index <= len(fetch.read().split("\n")):
+            if fetch.read().split("\n")[index].split("|||")[1] == parameters.remove:
+                subprocess.call("sudo atrm " + fetch.read().split("\n")[index].split("|||")[0])
+                regenerated = fetch.read().split("\n")
+                regenerated.remove(fetch.read().split("\n")[index].split("|||")[0] + "|||" + fetch.read().split("\n")[index].split("|||")[1])
+            pass
+        pass
+    pass
+    remove("/etc/deargodpleaseno/entries")
+    with open("/etc/deargodpleaseno/entries", "w") as rebuild:
+        rebuild.writelines(regenerated)
+    pass
+    print("Removed item.")
 pass
--- src/DEBIAN/usr/bin/deargodpleaseno/remove.py
+++
@@ -1,6 +0,0 @@
-"""
-deargodpleaseno, revision 1.0
-
-remove.py, does the following:
-- removes items from dump and atq
-"""
